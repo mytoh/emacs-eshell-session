e5d9415f0fba84720221c8ddea70862b262ca9e9
mytoh <mytoh@gmail.com>
2014-11-12 01:56:28 +0900

    * eshell-session.el: remove copied document

--- eshell-session.el
+++ eshell-session.el
@@ -105,14 +105,6 @@
 
 
 (defun eshell-session:eshell (&optional arg)
-  "Create an interactive Eshell buffer.
-The buffer used for Eshell sessions is determined by the value of
-`eshell-buffer-name'.  If there is already an Eshell session active in
-that buffer, Emacs will simply switch to it.  Otherwise, a new session
-will begin.  A numeric prefix arg (as in `C-u 42 M-x eshell RET')
-switches to the session with that number, creating it if necessary.  A
-nonnumeric prefix arg means to create a new session.  Returns the
-buffer selected (or created)."
   (interactive "P")
   (cl-assert eshell-buffer-name)
   (let ((buf (cond ((numberp arg)
